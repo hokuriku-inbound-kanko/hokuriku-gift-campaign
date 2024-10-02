02a95bd01e596e1b882dcd935d5d473ba74aa992
yamaji <yamaji@jig.jp>
2024-10-02 11:37:50 +0900

    👍 更新用のスクリプトを修正

--- update.sh
+++ update.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 
-# dotenvファイルの読み込み
+#################### variables ####################
+#shellcheck disable=SC2034
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+MAGENTA='\033[35m'
+CYAN='\033[36m'
+WHITE='\033[37m'
+GRAY='\033[38;5;244m'
+RESET='\033[0m'
+
+#################### main ####################
+echo -e "${CYAN}auto update hokuriku-gift-campain data${RESET}"
+
+# read dotenv
 eval "$(cat .env <(echo) <(declare -x))"
 
-# sftpでサーバーからデータを取得
+# get data with sftp
 cd daily
 DATE=$(date -d '1 days ago' '+%Y-%m-%d')
-expect -c "spawn sftp ${SFTP_USER}@${SFTP_HOST}
-expect \"password: \"
-send \"${SFTP_PASSWORD}\"
-expect \"sftp> \"
+echo -e "${CYAN}get $DATE.csv${RESET}"
+expect -d -c "
+spawn sftp ${SFTP_USER}@${SFTP_HOST}
+expect \"${SFTP_USER}@${SFTP_HOST}'s password:\"
+send \"${SFTP_PASSWORD}\r\"
+expect \"sftp>\"
 send \"get ${SFTP_BASEPATH}/${DATE}.csv\r\"
-expect \"sftp> \"
+expect \"sftp>\"
 send \"quit\r\"
 "
 
-# shift-jisのcsvをutf-8に変換
+# convert shift-jis to utf-8
+echo -e "${CYAN}convert data charset${RESET}"
 cd ..
 deno run -A conv-charset.ts --path="daily/${DATE}.csv" --from=shift-jis
+if [ $? -ne 0 ]; then
+  echo -e "${RED}ERROR:${RESET} convert data failed"
+  exit 1
+fi
 
-# commitしてpush
+# commit & push
+echo -e "${CYAN}upload to github${RESET}"
 git add -A
-git commit -m "🤖 update data"
+git commit -m "?~_?~V update data"
 git push
