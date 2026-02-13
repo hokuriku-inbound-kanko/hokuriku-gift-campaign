5edfd2d1a237004590cf5dbed45edfda94306c62
kuwabara <kuwabara@jig.jp>
2026-02-13 17:09:50 +0900

    🚑 文字化けしてしまったのを修正した

--- update.sh
+++ update.sh
@@ -35,13 +35,20 @@ expect \"sftp>\"
 send \"quit\r\"
 "
 
-# convert shift-jis to utf-8
-echo -e "${CYAN}convert data charset${RESET}"
 cd ..
-deno run -A conv-charset.ts --path="daily/${DATE}.csv" --from=shift-jis
-if [ $? -ne 0 ]; then
-  echo -e "${RED}ERROR:${RESET} convert data failed"
-  exit 1
+if ! file -i "daily/${DATE}.csv" | grep -q "charset=utf-8"; then
+  # convert shift-jis to utf-8
+  echo -e "${CYAN}convert data charset${RESET}"
+  deno run -A conv-charset.ts --path="daily/${DATE}.csv" --from=shift-jis
+
+  if [ $? -ne 0 ]; then
+    echo -e "${RED}ERROR:${RESET} convert data failed"
+    exit 1
+  fi
+  
+else
+  # skip convert 
+  echo -e "${CYAN}File is already UTF-8. Skipping conversion.${RESET}"
 fi
 
 # commit & push
